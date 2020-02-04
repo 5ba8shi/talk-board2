8cd21542724aa3d4e4172db8afe8fb60312eb4cb
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-04 16:34:54 +0900

    う

--- index.php
+++ index.php
@@ -16,10 +16,11 @@
 
   if (!empty($_POST)) {
     if ($_POST['message'] !== '') {
-      $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, created=NOW()');
+      $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
       $message->execute(array(
         $member['id'],
-        $_POST['message']
+        $_POST['message'],
+        $_POST['reply_post_id']
       ));
 
       header('Location: index.php');
@@ -28,6 +29,15 @@
   }
 
   $posts = $db->query('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
+
+  if (isset($_REQUEST['res'])) {
+    // 返信の処理
+    $response = $db->prepare('SELECT m.name, m.picture,p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=?');
+    $response->execute(array($_REQUEST['res']));
+
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table['message'];
+  }
 ?>
 
 
@@ -54,8 +64,8 @@
       <dl>
         <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES)); ?>さん、メッセージをどうぞ</dt>
         <dd>
-          <textarea name="message" cols="50" rows="5"></textarea>
-          <input type="hidden" name="reply_post_id" value="" />
+          <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+          <input type="hidden" name="reply_post_id" value= "<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?>" />
         </dd>
       </dl>
       <div>
